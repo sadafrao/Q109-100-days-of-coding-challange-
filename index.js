a699474842da49f5e37a5aa844af940a8cab2aca
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 16:38:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// Q109
+// check the current hours and log good morning if it is a before 12pm
+var time = new Date();
+if (time.getHours() < 12) {
+    console.log("Good Morning");
+}
+;
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Q109
+// check the current hours and log good morning if it is a before 12pm
+
+
+let time = new Date()
+if (time.getHours() < 12){
+    console.log("Good Morning")
+}
+    ;
